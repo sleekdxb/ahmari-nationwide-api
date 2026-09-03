b7d7b6f276e996517ea77d966ba310486e5c2efd
Farazdaq <frzdg6@gmail.com>
2026-09-03 14:20:43 +0400

    Add Vehicle Filtering and Pagination API #19

--- app/Http/Controllers/VehicleController.php
+++ app/Http/Controllers/VehicleController.php
@@ -104,6 +104,43 @@ class VehicleController extends Controller
     }
 
 
+    public function filterVehicle(Request $request): JsonResponse
+    {
+        $validator = Validator::make($request->all(), [
+            'per_page' => 'nullable|string',
+            'page' => 'nullable|string',
+            'vin' => 'nullable|string',
+            'year' => 'nullable|integer',
+            'make' => 'nullable|string',
+            'model' => 'nullable|string',
+            'trim' => 'nullable|string',
+            'condition' => 'nullable|string',
+            'body_type' => 'nullable|string',
+            'transmission' => 'nullable|string',
+            'fuel_type' => 'nullable|string',
+            'mileage' => 'nullable|integer',
+            'engine' => 'nullable|string',
+            'drivetrain' => 'nullable|string',
+            'exterior_color' => 'nullable|string',
+            'interior_color' => 'nullable|string',
+            'doors' => 'nullable|integer',
+            'seats' => 'nullable|integer',
+            'location' => 'nullable|string',
+            'price' => 'nullable|numeric|min:0',
+            'description' => 'nullable|string',
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json([
+                'success' => false,
+                'errors' => $validator->errors()
+            ], 422);
+        }
+
+        return VehicleHelper::filterVehicle($request);
+    }
+
+
 
     public function deleteVehicle(Request $request): JsonResponse
     {
